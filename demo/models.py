b4be415d97194476f963eccec2927964a233e76a
Flora_ssss <florasy98@gmail.com>
2021-12-15 09:44:06 -0800

    lecture 14-16

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    # field: 
+    # unique: title would be unique in db
+    # choice: limit the title you can choose 
+    # null, blank ,, 
+
+    title = models.CharField(max_length=36, unique=True)
+    description = models.TextField(max_length=256, blank = True)
+    price = models.DecimalField(default=0.0)
+    published = models.DateField(auto_now=True, auto_now_add=True)
